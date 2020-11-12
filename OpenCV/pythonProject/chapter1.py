6c1e4513b46d6894ec8008d9610c2e9d5ed9d23f
Mukund1985 <mukund.pandey@gmail.com>
2020-11-12 09:15:31 +0530

    Updating Latest files

--- OpenCV/pythonProject/chapter1.py
+++ OpenCV/pythonProject/chapter1.py
@@ -1,8 +1,27 @@
 import cv2
+import numpy as np
 
 print("Package Imported")
 
+##################################
 # Read - Images - Videos - WebCam
+##################################
+
+####################
+# To Read the Image
+####################
+
+img = cv2.imread("Resources/lena.png")
+
+#######################
+# To Display the Image
+#######################
+
+cv2.imshow("Output", img)
+cv2.waitKey(1000)
+
+##########################
+# How to Import the Video
+##########################
 
 
-img = cv2.imread()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DeepLearning
+
+This Repository is for all different type of projects and personal tutorial as well as work I have completed in DeepLearning with OpenCV. 
